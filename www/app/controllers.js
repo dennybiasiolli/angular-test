b83748540809e02abe13f1abf01d9f9ad4ab475a
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2015-03-25 21:51:26 +0100

    First commit

---
+++ www/app/controllers.js
@@ -0,0 +1,62 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', [])
+    //.controller('TestCtrl1', ['$scope', '$location', function($scope, $location) {
+    //    $scope.initialized = false;
+    //    $scope.InitButton = "Inizializza le variabili di scope";
+    //    $scope.init = function(){
+    //        $scope.titolo = "TITOLO INIT";
+    //        $scope.lista = ['elem1', 'elem2', 'elem3'];
+    //        $scope.initialized = true;
+    //    }
+    //    $scope.resetInit = function(){
+    //        $scope.titolo = "";
+    //        $scope.lista = null;
+    //        $scope.initialized = false;
+    //    }
+    //    $scope.goToSearch = function(){
+    //        $location.path("search");
+    //    }
+    //}])
+
+    //.controller('SearchController', ['$scope', '$routeParams', 'NewsRest', function($scope, $routeParams, NewsRest) {
+    //    $scope.searchKey = $routeParams.key;
+//
+    //    $scope.news = [
+    //        {
+    //            title: 'Titolo prima news',
+    //            description: 'Questa è la prima descrizione.',
+    //            timestamp: '1426848122'
+    //        },
+    //        {
+    //            title: 'Seconda news',
+    //            description: 'La seconda news è questa.',
+    //            timestamp: '1420878122'
+    //        },
+    //        {
+    //            title: 'Terzo titolo',
+    //            description: 'Descrizione nella terza news.',
+    //            timestamp: '1413838122'
+    //        }
+    //    ];
+
+    //    console.log(NewsRest);
+    //    NewsRest.getNews().
+    //    success(function(data, status, headers, config) {
+    //        $scope.news = data;
+    //    }).
+    //    error(function(data, status, headers, config) {
+    //        $scope.news = null;
+    //    });
+
+    //    $scope.inserisciArticolo = function(articolo){
+    //        $scope.news.push({
+    //            title: articolo.title,
+    //            description: articolo.description,
+    //            timestamp: (new Date()) / 1000
+    //        });
+    //    }
+    //}])
+;
