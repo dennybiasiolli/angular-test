20220ba72922f2e9d9b5eb62b606daf4f7a5dbbf
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2015-05-20 18:32:37 +0200

    Aggiunta app AVIS

---
+++ www/app/components/avis/services.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/* Services */
+
+
+// Demonstrate how to register services
+// In this case it is a simple value service.
+angular.module('myApp.avis')
+    .service('DonatoriRest', ['$http', function($http){
+        var service = {};
+        service.getDonatori = function(filtri){
+            return $http.get('app/components/avis/donatori.json', {filtri: filtri});
+        };
+        return service;
+    }]);
+;
